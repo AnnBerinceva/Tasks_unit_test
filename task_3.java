9784fc8ee13fc9c003faa49879ada01a9370d53f
AnnBerinceva <118420595+AnnBerinceva@users.noreply.github.com>
2024-08-11 21:16:07 +0500

    Update task_3.java

--- task_3.java
+++ task_3.java
@@ -1,15 +1,15 @@
-@RunWith(Parameterized.class) // Напиши аннотацию для параметризованных тестов
+@RunWith(Parameterized.class)
 public class CheckIsAdultTest {
   private final int age;
   private final boolean result;
   public CheckIsAdultTest(int age, boolean result) {
-	this.age = age; // Инициализируй поля класса в конструкторе
+	this.age = age;
     this.result = result; }
-  @Parameterized.Parameters // Пометь метод аннотацией для параметров
+  @Parameterized.Parameters
   public static Object[][] getTextData() {
 	return new Object[][] {
 		{ 18, true},
-        { 20, true}, // Заполни массив тестовыми данными и ожидаемым результатом
+        { 20, true}, 
         { 21, true},
         { 22, true},
     };
@@ -17,10 +17,7 @@ public class CheckIsAdultTest {
   @Test
   public void checkIsAdultWhenAgeThenResult() {
 	Program program = new Program();
-	 // Передай сюда возраст пользователя
     boolean isAdult = program.checkIsAdult(age);
-	// Сравни полученный и ожидаемый результаты, не забудь про сообщение об ошибке
-    assertEquals("Должно вернуться true, так как пользователь совершеннолетний", true, isAdult);
+    assertEquals("true, так как пользователь совершеннолетний", true, isAdult);
 	}
 }
-
