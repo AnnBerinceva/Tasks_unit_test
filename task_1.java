5942c50064b0fbc267db5537f7882994a0ad3f22
AnnBerinceva <118420595+AnnBerinceva@users.noreply.github.com>
2024-08-11 21:15:13 +0500

    Update task_1.java

--- task_1.java
+++ task_1.java
@@ -2,5 +2,4 @@
 public void checkIsAdultWhenAgeIsMoreThan18True() {
     Program program = new Program();
     boolean isAdult = program.checkIsAdult(19);
-	assertEquals("понятный текст ошибки", true, isAdult);// Напиши код здесь
-}
+	assertEquals("понятный текст ошибки", true, isAdult);
